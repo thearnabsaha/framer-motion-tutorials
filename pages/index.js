c2e757d30fdb67e8a4f609e148df88960559e2c4
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-11 13:14:32 +0530

    s1

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import { motion } from "framer-motion"
 const index = () => {
   return (
-    <div>
-      Enter
-    </div>
+    <motion.div animate={{marginTop:"20px"}}>
+      <motion.h2 initial={{x:"100vw",y:-10}} animate={{fontSize:"50px",color:"#ff2994",x:100,y:-10}}transition={{delay:1,duration:5,type:"spring",stiffness:120}} >arnab saha</motion.h2>
+      <motion.button animate={{rotateZ:180}} >submit</motion.button>
+    </motion.div>
   );
 }
 
