89f49767f6f7939a33eb0703a026b79cff5f9b13
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-11 13:55:05 +0530

    s1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import { motion } from "framer-motion"
+const buttonAnimation={
+  whileHover:{scale:1.5,backgroundColor:"red",color:"white",border:"none"},
+  ending:{x:100},
+}
+const headingAnimation={
+  starting:{
+    x:"100vw",y:-10
+  },
+  ending:{fontSize:"50px",color:"#ff2994",x:100,y:-10, transition:{delay:1,duration:5,type:"spring",stiffness:120}},
+  
+}
 const index = () => {
   return (
     <motion.div animate={{marginTop:"20px"}}>
-      <motion.h2 initial={{x:"100vw",y:-10}} animate={{fontSize:"50px",color:"#ff2994",x:100,y:-10}}transition={{delay:1,duration:5,type:"spring",stiffness:120}} >arnab saha</motion.h2>
-      <motion.button animate={{rotateZ:180}} >submit</motion.button>
+      <motion.h2 variants={headingAnimation} initial="starting" animate="ending">arnab saha</motion.h2>
+      <motion.button variants={buttonAnimation} animate="ending">submit</motion.button>
     </motion.div>
   );
 }
