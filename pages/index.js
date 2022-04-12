2d34b5d4decc800b8584c45bebade9293e754c99
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-12 07:20:01 +0530

    s2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-import { motion } from "framer-motion"
-const buttonAnimation={
-  whileHover:{scale:1.5,backgroundColor:"red",color:"white",border:"none"},
-  ending:{x:100},
-}
+import { motion,AnimatePresence } from "framer-motion"
+import { useState } from "react"
+import Link from 'next/link';
 const headingAnimation={
   starting:{
     x:"100vw",y:-10
@@ -10,10 +8,13 @@ const headingAnimation={
   ending:{fontSize:"50px",color:"#ff2994",x:100,y:-10, transition:{delay:1,duration:5,type:"spring",stiffness:120}},
 }
 const index = () => {
+  const [showTitle, setShowTitle] = useState(true)
   return (
     <motion.div animate={{marginTop:"20px"}}>
       <motion.h2 variants={headingAnimation} initial="starting" animate="ending">arnab saha</motion.h2>
-      <motion.button variants={buttonAnimation} animate="ending">submit</motion.button>
+      <motion.button animate={{x:100}} whileHover={{scale:1.5,backgroundColor:"red",color:"white",border:"none", transition:{yoyo:Infinity,duration:0.4}}} onClick={()=>{setShowTitle(false)}}>Go</motion.button>
+      <motion.button animate={{x:100}} whileHover={{scale:1.5,backgroundColor:"red",color:"white",border:"none", transition:{yoyo:Infinity,duration:0.4}}} onClick={()=>{setShowTitle(true)}}>Come</motion.button>
+      <AnimatePresence>{showTitle && (<motion.h3 exit={{y:-1000,transition:{duration:1}}} initial={{y:-1000}} animate={{x:100,y:1}} transition={{duration:1,type:"tween"}}>pokemon</motion.h3>)}</AnimatePresence>
     </motion.div>
   );
 }
